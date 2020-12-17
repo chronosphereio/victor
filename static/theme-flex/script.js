c7cfdda46c297ec8ab10c4dcb0226730f54315ef
Chris Chinchilla <chris@chronosphere.io>
2020-12-17 17:56:37 +0100

    Add clipboard js back

--- static/theme-flex/script.js
+++ static/theme-flex/script.js
@@ -117,64 +117,64 @@ jQuery(document).ready(function() {
 
     // clipboard
     // TODO: Bring back
-    // var clipInit = false;
-    // $('pre code').each(function() {
-    //     var code = $(this),
-    //         text = code.text();
+    var clipInit = false;
+    $('pre code').each(function() {
+        var code = $(this),
+            text = code.text();
 
-    //     if (text.length > 5) {
-    //         if (!clipInit) {
-    //             var text, clip = new Clipboard('.copy-to-clipboard', {
-    //                 text: function(trigger) {
-    //                     text = $(trigger).next('code').text();
-    //                     return text.replace(/^\$\s/gm, '');
-    //                 }
-    //             });
+        if (text.length > 5) {
+            if (!clipInit) {
+                var text, clip = new ClipboardJS('.copy-to-clipboard', {
+                    text: function(trigger) {
+                        text = $(trigger).next('code').text();
+                        return text.replace(/^\$\s/gm, '');
+                    }
+                });
 
-    //             var inPre;
-    //             clip.on('success', function(e) {
-    //                 e.clearSelection();
-    //                 inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-    //                 $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-    //             });
+                var inPre;
+                clip.on('success', function(e) {
+                    e.clearSelection();
+                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
+                    $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+                });
 
-    //             clip.on('error', function(e) {
-    //                 inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-    //                 $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-    //                 $(document).one('copy', function(){
-    //                     $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-    //                 });
-    //             });
+                clip.on('error', function(e) {
+                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
+                    $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+                    $(document).one('copy', function(){
+                        $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+                    });
+                });
 
-    //             clipInit = true;
-    //         }
+                clipInit = true;
+            }
 
-    //         code.before('<span class="copy-to-clipboard" title="Copy to clipboard"><span class="fa fa-clipboard" aria-hidden="true"></span></span>');
-    //         $('.copy-to-clipboard').on('mouseleave', function() {
-    //             $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
-    //         });
-    //     }
-    // });
+            code.before('<span class="copy-to-clipboard" title="Copy to clipboard"><span class="fa fa-clipboard" aria-hidden="true"></span></span>');
+            $('.copy-to-clipboard').on('mouseleave', function() {
+                $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+            });
+        }
+    });
 
     // allow keyboard control for prev/next links
-    jQuery(function() {
-        jQuery('.nav-prev').click(function(){
-            location.href = jQuery(this).attr('href');
-        });
-        jQuery('.nav-next').click(function() {
-            location.href = jQuery(this).attr('href');
-        });
-    });
-    jQuery(document).keydown(function(e) {
-      // prev links - left arrow key
-      if(e.which == '37') {
-        jQuery('.nav.nav-prev').click();
-      }
-      // next links - right arrow key
-      if(e.which == '39') {
-        jQuery('.nav.nav-next').click();
-      }
-    });
+    // jQuery(function() {
+    //     jQuery('.nav-prev').click(function(){
+    //         location.href = jQuery(this).attr('href');
+    //     });
+    //     jQuery('.nav-next').click(function() {
+    //         location.href = jQuery(this).attr('href');
+    //     });
+    // });
+    // jQuery(document).keydown(function(e) {
+    //   // prev links - left arrow key
+    //   if(e.which == '37') {
+    //     jQuery('.nav.nav-prev').click();
+    //   }
+    //   // next links - right arrow key
+    //   if(e.which == '39') {
+    //     jQuery('.nav.nav-next').click();
+    //   }
+    // });
 
     $('article a:not(:has(img)):not(.btn)').addClass('highlight');
 });
